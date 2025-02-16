1370b885a7b70c29a86417af299edd52d05d0543
mouwrice <mauricevanwassenhove@fastmail.com>
2025-02-16 12:22:06 +0100

    update, bat, nixd, fmt

--- flake.nix
+++ flake.nix
@@ -3,19 +3,30 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    envycontrol.url = github:bayasdev/envycontrol;
+    envycontrol = {
+      url = "github:bayasdev/envycontrol";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, envycontrol, ... }:
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      envycontrol,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
+      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
       homeConfigurations."mouwrice" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         extraSpecialArgs = { inherit envycontrol; };
--- home.nix
+++ home.nix
@@ -1,12 +1,13 @@
-{ config, pkgs, envycontrol, ... }:
+{
+  pkgs,
+  envycontrol,
+  ...
+}:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "mouwrice";
   home.homeDirectory = "/var/home/mouwrice";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   # This value determines the Home Manager release that your configuration is
@@ -17,12 +18,12 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
- 
+
   programs.zsh = {
     enable = true;
     autocd = true;
     shellAliases = {
-     hm = "home-manager --flake /var/home/mouwrice/.dotfiles/";
+      hm = "home-manager --flake /var/home/mouwrice/.dotfiles/";
     };
   };
 
@@ -33,44 +34,18 @@
   programs.pay-respects.enable = true;
 
   home.packages = [
-   pkgs.rustup
-   pkgs.pkg-config
-   pkgs.openssl
-   pkgs.podman-compose
-   envycontrol.packages.x86_64-linux.default
+    pkgs.rustup
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.podman-compose
+    pkgs.nixd
+    pkgs.bat
+    envycontrol.packages.x86_64-linux.default
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/mouwrice/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
