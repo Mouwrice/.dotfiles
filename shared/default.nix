07f7c3f5d684efe49ce05dd019f9b61fc1546ff5
mouwrice <mauricevanwassenhove@fastmail.com>
2025-10-13 20:18:34 +0200

    feat: add shared module

---
+++ shared/default.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+{
+
+  home.packages = with pkgs; [
+    fastfetch
+    nixd
+    bat
+    sbt
+    nvd
+    presenterm
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
+
+    fish = {
+      enable = true;
+      generateCompletions = true;
+      shellInit = ''
+      fish_config theme choose "ayu Dark"
+        '';
+    };
+
+    starship = {
+      enable = true;
+      settings = {
+        # "$schema" = 'https://starship.rs/config-schema.json'
+        command_timeout = 2000;
+      };
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+  };
+}
