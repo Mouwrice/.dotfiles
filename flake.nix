e413b1e0012d520084c9bed1e7c567a614573612
Maurice Van Wassenhove <mauricevanwassenhove@fastmail.com>
2025-10-23 19:49:41 +0200

    init

--- flake.nix
+++ flake.nix
@@ -1,36 +1,53 @@
 {
-  description = "Home Manager configuration of mouwrice";
+  description = "An NixOS flake template that you can adapt to your own environment";
 
+  # Flake inputs
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs
+    determinate = {
+      url = "https://flakehub.com/f/DeterminateSystems/determinate/3"; # Determinate 3.*
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    envycontrol = {
-      url = "github:bayasdev/envycontrol";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
+  # Flake outputs
   outputs =
+    { self, ... }@inputs:
     {
-      nixpkgs,
-      home-manager,
-      envycontrol,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      formatter.x86_64-linux = pkgs.nixfmt-rfc-style;
-      homeConfigurations."mouwrice" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit envycontrol; };
-        modules = [ ./home.nix ];
+      # A minimal (but updatable!) NixOS configuration output by this flake
+      nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+        # Change this if you're building for a system type other than x86 AMD Linux
+        system = "x86_64-linux";
+
+        modules = [
+          # Load the Determinate module, which provides Determinate Nix
+          inputs.determinate.nixosModules.default
+          inputs.nixos-hardware.nixosModules.framework-amd-ai-300-series
+
+          # Load the hardware configuration from a separate file (a common convention for NixOS)
+          ./hardware-configuration.nix
+
+          ./configuration.nix
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mouwrice = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+
+        specialArgs = {
+          # Values to pass to modules
+        };
       };
     };
 }
+
