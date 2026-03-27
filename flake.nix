796bdf1747c47dd5b596ceceaf9ac3504850601d
mouwrice <mauricevanwassenhove@fastmail.com>
2026-03-27 14:33:54 +0100

    feat: setup home-manager

--- flake.nix
+++ flake.nix
@@ -1,50 +1,31 @@
 {
-  description = "An NixOS flake template that you can adapt to your own environment";
+  description = "Home Manager configuration of mouwrice";
 
-  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Stable Nixpkgs
-    determinate = {
-      url = "https://flakehub.com/f/DeterminateSystems/determinate/3"; # Determinate 3.*
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  # Flake outputs
   outputs =
-    { self, nixpkgs, home-manager, nixos-hardware, determinate, ... }:
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
-      # A minimal (but updatable!) NixOS configuration output by this flake
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        # Change this if you're building for a system type other than x86 AMD Linux
-        system = "x86_64-linux";
+      homeConfigurations."mouwrice" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-        modules = [
-          # Load the Determinate module, which provides Determinate Nix
-          determinate.nixosModules.default
-          nixos-hardware.nixosModules.framework-amd-ai-300-series
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
-          # Load the hardware configuration from a separate file (a common convention for NixOS)
-          ./hardware-configuration.nix
-
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mouwrice = import ./home.nix;
-          }
-        ];
-
-        specialArgs = {
-          # Values to pass to modules
-        };
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
       };
     };
 }
-
